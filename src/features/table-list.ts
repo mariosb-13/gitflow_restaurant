12a24c6bfc8e2f7ac81e9f27e76acf24ed817b97
David Roman Rodriguez <dromrod529@iescarrillo.es>
2025-10-01 12:50:26 +0200

    Funcionalidad Table-List finished

--- src/features/table-list.ts
+++ src/features/table-list.ts
@@ -6,3 +6,17 @@ numero: number;
 capacidad: number;
 ocupada: boolean;
 }
+
+let mesas: Mesa[] = [
+{ numero: 1, capacidad: 2, ocupada: false },
+{ numero: 2, capacidad: 4, ocupada: true },
+{ numero: 3, capacidad: 6, ocupada: false }
+];
+export function mostrarMesasDisponibles(): void {
+console.log(" Mesas disponibles:");
+mesas.forEach(mesa => {
+if (!mesa.ocupada) {
+console.log(`Mesa ${mesa.numero} (Capacidad: ${mesa.capacidad})`);
+}
+});
+}
