92950434c5cb3324f9dfe8619ed8b789df6b4b2b
mariosb_13 <msanbar104g@iescarrillo.es>
2025-10-01 12:52:54 +0200

    Funcionalidad Login

---
+++ src/feature/login.ts
@@ -0,0 +1,11 @@
+// -----------------------------
+// Feature: Login
+// -----------------------------
+interface Usuario {
+email: string;
+password: string;
+}
+const usuarios: Usuario[] = [
+{ email: "ana@example.com", password: "1234" },
+{ email: "juan@example.com", password: "abcd" }
+];
