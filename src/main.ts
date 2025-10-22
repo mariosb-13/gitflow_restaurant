3e997f5b8e876700be6d4f0ae63ee3ea2f9117ae
mariosb_13 <msanbar104g@iescarrillo.es>
2025-10-22 12:32:46 +0200

    Nueva funcionalidad Client Discount

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,15 @@ if (acceso) {
 } else {
     console.log("Acceso denegado: credenciales incorrectas");
 }
+
+export function procesarReserva(cliente: string, total: number): string {
+    console.log(`Procesando reserva para ${cliente}.`);
+    let descuento = 0;
+    if (cliente === "Juan Pérez") {
+        descuento = total * 0.1;
+        total -= descuento;
+        console.log(` Descuento aplicado de ${descuento.toFixed(2)} €`);
+    }
+    console.log(`Total a pagar: ${total} €`);
+    return "Reserva completada correctamente con descuento.";
+}
