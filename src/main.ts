8ce7c3d519ace7125945e6e7e9363a150e99f2e5
mariosb_13 <msanbar104g@iescarrillo.es>
2025-10-22 10:55:22 +0200

    New function procesarReserva

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,20 @@ import { mostrarMesasDisponibles } from "./feature/table-list";
 console.log("===== SISTEMA DE RESERVAS v1.0.0 =====");
 const acceso = login("ana@example.com", "1234");
 if (acceso) {
-console.log("Acceso concedido con éxito ");
-mostrarMesasDisponibles();
+    console.log("Acceso concedido con éxito ");
+    mostrarMesasDisponibles();
 } else {
-console.log("Acceso denegado: credenciales incorrectas");
+    console.log("Acceso denegado: credenciales incorrectas");
+}
+
+export function procesarReserva(cliente: string, total: number): string {
+    console.log(`Procesando reserva para ${cliente}.`);
+    let descuento = 0;
+    if (cliente === "Juan Pérez") {
+        descuento = total * 0.1;
+        total -= descuento;
+        console.log(` Descuento aplicado de ${descuento.toFixed(2)} €`);
+    }
+    console.log(`Total a pagar: ${total} €`);
+    return "Reserva completada correctamente con descuento.";
 }
