abe4b7e514883c6738729de8736602696b021d8c
David Roman Rodriguez <dromrod529@iescarrillo.es>
2025-10-22 10:55:27 +0200

    implements new feature client-rewards

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,11 @@ mostrarMesasDisponibles();
 } else {
 console.log("Acceso denegado: credenciales incorrectas");
 }
+export function procesarReserva(cliente: string, total: number): string {
+ console.log(`Procesando reserva para ${cliente}.`);
+ console.log(`Total a pagar: ${total} €`);
+ let puntosGanados = Math.floor(total / 10);
+ console.log(` El cliente ${cliente} ha ganado ${puntosGanados} puntos.`);
+
+ return "Reserva completada correctamente con puntos de recompensa.";
+}
