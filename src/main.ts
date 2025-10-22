7cc367c97ad789cdbe8fe8d68f6fbb34b6392e1b
mariosb_13 <msanbar104g@iescarrillo.es>
2025-10-22 12:25:48 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,3 @@ if (acceso) {
 } else {
     console.log("Acceso denegado: credenciales incorrectas");
 }
-
-export function procesarReserva(cliente: string, total: number): string {
-    console.log(`Procesando reserva para ${cliente}.`);
-    let descuento = 0;
-    if (cliente === "Juan Pérez") {
-        descuento = total * 0.1;
-        total -= descuento;
-        console.log(` Descuento aplicado de ${descuento.toFixed(2)} €`);
-    }
-    console.log(`Total a pagar: ${total} €`);
-    return "Reserva completada correctamente con descuento.";
-}
