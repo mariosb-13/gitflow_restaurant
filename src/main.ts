8cead359885fe49d8d3878d449d8f77002b98d07
mariosb_13 <msanbar104g@iescarrillo.es>
2025-10-22 12:31:33 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,3 @@ if (acceso) {
 } else {
     console.log("Acceso denegado: credenciales incorrectas");
 }
-export function procesarReserva(cliente: string, total: number): string {
- console.log(`Procesando reserva para ${cliente}.`);
- console.log(`Total a pagar: ${total} €`);
- let puntosGanados = Math.floor(total / 10);
- console.log(` El cliente ${cliente} ha ganado ${puntosGanados} puntos.`);
-
- return "Reserva completada correctamente con puntos de recompensa.";
-}
